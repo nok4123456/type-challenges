4270812cc1710003704401068fdb893105f89b62
nok4123456 <nok4123456@gmail.com>
2023-05-13 17:46:53 +0800

    finish tuple question

--- playground/00011-easy-tuple-to-object.ts
+++ playground/00011-easy-tuple-to-object.ts
@@ -20,19 +20,38 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<Types extends readonly any[]> = {
+    [Type in Types[number]]: Type
+}
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const
 const tupleNumber = [1, 2, 3, 4] as const
-const tupleMix = [1, '2', 3, '4'] as const
+const tupleMix = [1, "2", 3, "4"] as const
 
 type cases = [
-  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
-  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
-  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
+    Expect<
+        Equal<
+            TupleToObject<typeof tuple>,
+            {
+                tesla: "tesla"
+                "model 3": "model 3"
+                "model X": "model X"
+                "model Y": "model Y"
+            }
+        >
+    >,
+    Expect<
+        Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>
+    >,
+    Expect<
+        Equal<
+            TupleToObject<typeof tupleMix>,
+            { 1: 1; "2": "2"; 3: 3; "4": "4" }
+        >
+    >
 ]
 
 // @ts-expect-error
